a408517d528285a665f4708a50d52a66283e8e92
Pabbineedi Ratna hema <119644628+Ratnahema@users.noreply.github.com>
2024-08-21 22:40:50 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        boolean playAgain = true;
+        while (playAgain) {
+            int lowerBound = 1;
+            int upperBound = 100;
+            int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            int maxAttempts = 10;
+            boolean guessedCorrectly = false;
+
+            System.out.println("Guess the number between " + lowerBound + " and " + upperBound + "!");
+            
+            while (attempts < maxAttempts && !guessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == randomNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    guessedCorrectly = true;
+                } else if (userGuess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've used all " + maxAttempts + " attempts. The correct number was " + randomNumber + ".");
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String response = scanner.next();
+            playAgain = response.equalsIgnoreCase("yes");
+        }
+
+        scanner.close();
+        System.out.println("Thank you for playing the Number Guessing Game!");
+    }
+}
